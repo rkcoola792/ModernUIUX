c314236a252a721cc403e5d3ed2c59fc036ad424
rkcoola792 <46224203+rkcoola792@users.noreply.github.com>
2023-12-25 16:08:52 +0530

    Folder structure completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-
+import { Footer,Blog,Possibilities,Features,WhatGPT3,Header } from "./containers"
+import { Cta,Brands,Navbar } from "./components"
 function App() {
 
   return (
-    <>
-      App
-    </>
+    <div className="App">
+    <div className="gradient_bg">
+      <Navbar/>
+      <Header/>
+    </div>
+    <Brands/>
+    <WhatGPT3/>
+    <Features/>
+    <Possibilities/>
+    <Cta/>
+    <Blog/>
+    <Footer/>
+    </div>
   )
 }
 
--- src/components/articles/Articles.jsx
+++ src/components/articles/Articles.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import "./articles.css"
+import "./article.css"
 const Articles = () => {
   return (
     <div>
