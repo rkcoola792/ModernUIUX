2d20f310f84be21aef68d7cec09a9d91d2247bda
rkcoola792 <46224203+rkcoola792@users.noreply.github.com>
2023-12-26 11:17:46 +0530

    navbar completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { Footer,Blog,Possibilities,Features,WhatGPT3,Header } from "./containers"
 import { Cta,Brands,Navbar } from "./components"
+import "./App.css"
 function App() {
 
   return (
     <div className="App">
-    <div className="gradient_bg">
+    <div className="gradient__bg">
       <Navbar/>
       <Header/>
     </div>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./navbar.css"
+import {RiCloseLine,RiMenu3Line } from "react-icons/ri"
+
+const Links=()=>{
+   return (
+     <>
+       <a href="#home">Home</a>
+       <a href="#wgpt3">What is GPT</a>
+       <a href="#possibilites">Open AI</a>
+       <a href="#features">Case Studies</a>
+       <a href="#blog">Library</a>
+     </>
+   );
+}
+
+
+
 const Navbar = () => {
+  const [toggleMenu,setToggleMenu]=useState(false);
   return (
-    <div>
-      Navbar
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="navbar-left-items">
+          <div className="gpt3__navbar-links-logo">
+            <img src="./src/assets/logo.svg" alt="" />
+          </div>
+          <div className="gpt3__navbar-links_container">
+            <Links />
+          </div>
+        </div>
+
+        <div className="gpt3__sigin_signup">
+          <a href="">Sign in</a>
+          <button href="">Sign up</button>
+        </div>
+        <div className="menu">
+          {!toggleMenu ? (
+            <RiMenu3Line
+              onClick={() => setToggleMenu(!toggleMenu)}
+              color="#fff"
+              size={30}
+            />
+          ) : (
+            <RiCloseLine
+              color="#fff"
+              size={35}
+              onClick={() => setToggleMenu(!toggleMenu)}
+            />
+          )}
+        </div>
+        {toggleMenu && (
+          <div className="sidebar-container">
+            <Links />
+          </div>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
 export default Navbar
